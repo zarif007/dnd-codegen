b087ef7cd1146fd7414303f3265567fff77e6f46
zarif007 <zarifhuq786@gmail.com>
2024-11-19 16:45:16 +0600

    feat: rete number node

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+import Rete from "./Rete";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Rete />
+    </main>
+  );
+}
